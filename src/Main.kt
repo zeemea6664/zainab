f30ccd63496ff939965ced8251863d5c560e52eb
zainab <zainabyusufbalogun@gmail.com>
2025-05-08 23:40:32 -0700

    task 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+//Write a Kotlin program that prints Hello, [Your Name]! to the console.
+ /*   fun main() {
+        val name = "Zainab Ahmad"
+        println("Hello, $name!")
+    }
+*/
 
-println("helo")
+//
+/*fun main() {
+    print("Please enter your name: ")
+    val name = readlnOrNull()
+
+    print("Please enter your age: ")
+    val ageString = readlnOrNull()
+    println("Hello $name, you are $ageString years old!")
+
+}*/
+
+//Write a program that checks if a given number is even or odd.
+fun main() {
+    print("Enter a number: ")
+    val number = readLine()?.toIntOrNull()
+
+    if (number != null) {
+        if (number % 2 == 0) {
+            println("$number is even")
+        } else {
+            println("$number is odd")
+        }
+    } else {
+        println("Please enter a valid number")
+    }
 }
+
